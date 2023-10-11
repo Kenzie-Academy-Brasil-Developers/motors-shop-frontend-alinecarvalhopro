c4396fb508ca3c32bd05b775e0bb647198ecc696
aline <alinecarvalho.pro@gmail.com>
2023-10-11 14:09:03 -0300

    feat: commet card component

---
+++ src/components/CommentCard/CommentCard.tsx
@@ -0,0 +1,19 @@
+import { TextBody2 } from "../Text/text.styled";
+import TagUser from "../fragments/TagUser/TagUser";
+import { StyledCommentCard } from "./commentCard.styled";
+
+const CommentCard = () => {
+  return (
+    <StyledCommentCard>
+      <TagUser char={"Seller Name".charAt(0).toLowerCase()}>Username</TagUser>
+      <TextBody2 margin="12px 0 0 0">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </TextBody2>
+    </StyledCommentCard>
+  );
+};
+
+export default CommentCard;
---
+++ src/components/CommentCard/commentCard.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledCommentCard = styled.li`
+  border: 1px solid red;
+
+  list-style: none;
+
+  display: flex;
+  flex-direction: column;
+`;
